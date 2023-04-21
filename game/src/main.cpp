cef9b96a3972de6f3556c7b745eb7b280e3b79cc
Miguel Yepes <miguelyegarcia94@gmail.com>
2023-04-21 12:27:08 -0600

    Check for map bounds created function to handle bounds.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,26 +2,31 @@
 #include "raymath.h"
 #include "Character.h"
 
-//Window Dimensions
-const int WINDOW_DIMENSION[2]{ 384, 384 };
 
 int main()
 {
+	//Window Dimensions
+	const int WINDOW_DIMENSION[2]{ 384, 384 };
+
 	InitWindow( WINDOW_DIMENSION[0], WINDOW_DIMENSION[1], "Hawlerak Clash" );
 
 	Character knight;
 
-	knight.setScreenPos(WINDOW_DIMENSION[0], WINDOW_DIMENSION[1]);
+
 
 	//Textures
 	Texture2D map = LoadTexture("nature_tileset/MapWorld.png");
 
+
 	//Positions
-	Vector2 mapStartPos{	
+	Vector2 mapPos{	
 		(-map.width * 2) + WINDOW_DIMENSION[0],
 		(0.0) }; //SelfNote: is negative widh becuase the image is the one that moves not the window, and time 2 'cause map is scaled by 2.
-	Vector2 mapPos{};
+	const float mapScale = 2.0f;
 	SetTargetFPS(60);
+
+	knight.setScreenPos(WINDOW_DIMENSION[0], WINDOW_DIMENSION[1]);
+	//knight.setWorldPos((Vector2Scale(knight.getWorldPos(), -1.0f)));
 
 	while (!WindowShouldClose())
 	{
@@ -30,13 +35,19 @@ int main()
 		BeginDrawing();
 		ClearBackground(WHITE);
 
-		mapPos = Vector2Add(Vector2Scale(knight.getWorldPos(), -1.0f), mapStartPos);
-		
+		mapPos = (Vector2Scale(knight.getWorldPos(), -1.0f));
 		//Maps
-		DrawTextureEx(map, mapPos, 0.0, 2.0, WHITE);
+		DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
 
 		knight.tick(dT);
 
+		//variables
+		bool isOutOfBound = knight.getWorldPos().x < 0.f ||
+							knight.getWorldPos().y < 0.f ||
+							knight.getWorldPos().x + WINDOW_DIMENSION[0] > map.width * mapScale ||
+							knight.getWorldPos().y + WINDOW_DIMENSION[1] > map.height * mapScale;
+		
+		if (isOutOfBound) knight.mapOutOfBound();
 		EndDrawing();
 	}
 	CloseWindow();
